7c08412336cb3d624c2342c79ba0d8ff9cbf1676
pedrofaino <pedrofaino99@gmail.com>
2023-04-26 19:29:32 -0300

    index modify

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ import authRouter from "./src/routes/auth.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import linkRouter from "./src/routes/link.route.js"
+import reminderRouter from "./src/routes/reminder.route.js"
+import { Reminder } from "./src/models/Reminder.js";
 
 const app = express();
 
-const whiteList = [process.env.ORIGIN1]
+const whiteList = [process.env.ORIGIN1,process.env.ORIGIN2]
 
 app.use(cors({
     origin:function(origin, callback){
-        if(whiteList.includes(origin)){
+        if(!origin||whiteList.includes(origin)){
             return callback(null, origin)
         }
         return callback("Error de CORS origin: "+origin+" no autorizado.")
-    }
-}));
+    },
+    credentials:true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +29,7 @@ app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/links', linkRouter);
+app.use('/api/v1/reminders', reminderRouter);
 
 
 const PORT = process.env.PORT || 5000;
